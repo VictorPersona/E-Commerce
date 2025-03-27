0278bed90c71c8b3c1520aa88b0d17cfb4ed80af
VictorPersona <gauravtupe00@gmail.com>
2025-03-27 09:09:34 +0530

    initialize React application with Vite setup

---
+++ frontend/src/assets/asset.js
@@ -0,0 +1,423 @@
+import img_01 from './Products/img_01.jpg'
+import img_02 from './Products/img_02.jpg'
+import img_03 from './Products/img_03.jpg'
+import img_04 from './Products/img_04.jpg'
+import img_05 from './Products/img_05.jpg'
+import img_06 from './Products/img_06.jpg'
+import img_07 from './Products/img_07.jpg'
+import img_08 from './Products/img_08.jpg'
+import img_09 from './Products/img_09.jpg'
+import img_10 from './Products/img_10.jpg'
+import img_11 from './Products/img_11.jpg'
+import img_12 from './Products/img_12.jpg'
+import img_13 from './Products/img_13.jpg'
+import img_14 from './Products/img_14.jpg'
+import img_15 from './Products/img_15.jpg'
+import img_16 from './Products/img_16.jpg'
+import img_17 from './Products/img_17.jpg'
+import img_18 from './Products/img_18.jpg'
+import img_19 from './Products/img_19.jpg'
+import img_20 from './Products/img_20.jpg'
+import img_21 from './Products/img_21.jpg'
+import img_22 from './Products/img_22.jpg'
+import img_23 from './Products/img_23.jpg'
+import img_24 from './Products/img_24.jpg'
+import img_25 from './Products/img_25.jpg'
+import img_26 from './Products/img_26.jpg'
+import img_27 from './Products/img_27.jpg'
+import img_28 from './Products/img_28.jpg'
+import img_29 from './Products/img_29.jpg'
+import img_30 from './Products/img_30.jpg'
+import img_31 from './Products/img_31.jpg'
+import img_32 from './Products/img_32.jpg'
+import img_33 from './Products/img_33.jpg'
+import img_34 from './Products/img_34.jpg'
+import img_35 from './Products/img_35.jpg'
+import img_36 from './Products/img_36.jpg'
+import img_37 from './Products/img_37.jpg'
+import img_38 from './Products/img_38.jpg'
+import img_39 from './Products/img_39.jpg'
+import img_40 from './Products/img_40.jpg'
+import img_41 from './Products/img_41.jpg'
+import img_42 from './Products/img_42.jpg'
+import img_43 from './Products/img_43.jpg'
+import img_44 from './Products/img_44.jpg'
+import img_45 from './Products/img_45.jpg'
+import img_46 from './Products/img_46.jpg'
+import img_47 from './Products/img_47.jpg'
+import img_48 from './Products/img_48.jpg'
+import img_49 from './Products/img_49.jpg'
+import img_50 from './Products/img_50.jpg'
+import img_51 from './Products/img_51.jpg'
+import img_52 from './Products/img_52.jpg'
+import img_53 from './Products/img_53.jpg'
+import img_54 from './Products/img_54.jpg'
+import img_55 from './Products/img_55.jpg'
+import img_56 from './Products/img_56.jpg'
+import img_57 from './Products/img_57.jpg'
+import img_58 from './Products/img_58.jpg'
+import img_59 from './Products/img_59.jpg'
+import img_60 from './Products/img_60.jpg'
+import img_61 from './Products/img_61.jpg'
+import img_62 from './Products/img_62.jpg'
+import img_63 from './Products/img_63.jpg'
+import img_64 from './Products/img_64.jpg'
+import img_65 from './Products/img_65.jpg'
+import img_66 from './Products/img_66.jpg'
+import img_67 from './Products/img_67.jpg'
+import img_68 from './Products/img_68.jpg'
+import img_69 from './Products/img_69.jpg'
+import img_70 from './Products/img_70.jpg'
+import img_71 from './Products/img_71.jpg'
+import img_72 from './Products/img_72.jpg'
+import img_73 from './Products/img_73.jpg'
+import img_74 from './Products/img_74.jpg'
+import img_75 from './Products/img_75.jpg'
+import img_76 from './Products/img_76.jpg'
+import img_77 from './Products/img_77.jpg'
+import img_78 from './Products/img_78.jpg'
+import img_79 from './Products/img_79.jpg'
+import img_80 from './Products/img_80.jpg'
+import img_81 from './Products/img_81.jpg'
+import img_82 from './Products/img_82.jpg'
+import img_83 from './Products/img_83.jpg'
+import img_84 from './Products/img_84.jpg'
+
+export const Products = [
+    {
+        id: '1',
+        name: 'product1',
+        description: 'product1 description',
+        price: 100,
+        image: [img_01, img_02, img_03],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['S', 'M', 'L'],
+        date: 1716634345448,
+        bestseller: true,
+    },
+    {
+        id: '2',
+        name: 'product2',
+        description: 'product2 description',
+        price: 200,
+        image: [img_04, img_05, img_06],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['M', 'L'],
+        date: 1716834349448,
+        bestseller: false,
+    },
+    {
+        id: '3',
+        name: 'product3',
+        description: 'product3 description',
+        price: 80,
+        image: [img_07, img_08, img_09],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['S', 'L'],
+        date: 1716684345448,
+        bestseller: true,
+    },
+    {
+        id: '4',
+        name: 'product4',
+        description: 'product4 description',
+        price: 120,
+        image: [img_10, img_11, img_12],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['M', 'L'],
+        date: 1716734345448,
+        bestseller: false,
+    },
+    {
+        id: '5',
+        name: 'product5',
+        description: 'product5 description',
+        price: 150,
+        image: [img_13, img_14, img_15],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['S', 'M'],
+        date: 1716934345448,
+        bestseller: true,
+    },
+    {
+        id: '6',
+        name: 'product6',
+        description: 'product6 description',
+        price: 90,
+        image: [img_16, img_17, img_18],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['M', 'L'],
+        date: 1716634345448,
+        bestseller: false,
+    },
+    {
+        id: '7',
+        name: 'product7',
+        description: 'product7 description',
+        price: 110,
+        image: [img_19, img_20, img_21],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['S', 'M', 'L'],
+        date: 1716734345448,
+        bestseller: true,
+    },
+    {
+        id: '8',
+        name: 'product8',
+        description: 'product8 description',
+        price: 180,
+        image: [img_22, img_23, img_24],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['M', 'L'],
+        date: 1716834345448,
+        bestseller: false,
+    },
+    {
+        id: '9',
+        name: 'product9',
+        description: 'product9 description',
+        price: 70,
+        image: [img_25, img_26, img_27],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['S', 'L'],
+        date: 1716934345448,
+        bestseller: true,
+    },
+    {
+        id: '10',
+        name: 'product10',
+        description: 'product10 description',
+        price: 130,
+        image: [img_28, img_29, img_30],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['M', 'L'],
+        date: 1716634345448,
+        bestseller: false,
+    },
+    {
+        id: '11',
+        name: 'product11',
+        description: 'product11 description',
+        price: 140,
+        image: [img_31, img_32, img_33],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['S', 'M'],
+        date: 1716734345448,
+        bestseller: true,
+    },
+    {
+        id: '12',
+        name: 'product12',
+        description: 'product12 description',
+        price: 95,
+        image: [img_34, img_35, img_36],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['M', 'L'],
+        date: 1716834345448,
+        bestseller: false,
+    },
+    {
+        id: '13',
+        name: 'product13',
+        description: 'product13 description',
+        price: 115,
+        image: [img_37, img_38, img_39],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['S', 'M', 'L'],
+        date: 1716934345448,
+        bestseller: true,
+    },
+    {
+        id: '14',
+        name: 'product14',
+        description: 'product14 description',
+        price: 170,
+        image: [img_40, img_41, img_42],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['M', 'L'],
+        date: 1716634345448,
+        bestseller: false,
+    },
+    {
+        id: '15',
+        name: 'product15',
+        description: 'product15 description',
+        price: 75,
+        image: [img_43, img_44, img_45],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['S', 'L'],
+        date: 1716734345448,
+        bestseller: true,
+    },
+    {
+        id: '16',
+        name: 'product16',
+        description: 'product16 description',
+        price: 125,
+        image: [img_46, img_47, img_48],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['M', 'L'],
+        date: 1716834345448,
+        bestseller: false,
+    },
+    {
+        id: '17',
+        name: 'product17',
+        description: 'product17 description',
+        price: 160,
+        image: [img_49, img_50, img_51],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['S', 'M'],
+        date: 1716934345448,
+        bestseller: true,
+    },
+    {
+        id: '18',
+        name: 'product18',
+        description: 'product18 description',
+        price: 85,
+        image: [img_52, img_53, img_54],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['M', 'L'],
+        date: 1716634345448,
+        bestseller: false,
+    },
+    {
+        id: '19',
+        name: 'product19',
+        description: 'product19 description',
+        price: 105,
+        image: [img_55, img_56, img_57],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['S', 'M', 'L'],
+        date: 1716734345448,
+        bestseller: true,
+    },
+    {
+        id: '20',
+        name: 'product20',
+        description: 'product20 description',
+        price: 190,
+        image: [img_58, img_59, img_60],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['M', 'L'],
+        date: 1716834345448,
+        bestseller: false,
+    },
+    {
+        id: '21',
+        name: 'product21',
+        description: 'product21 description',
+        price: 65,
+        image: [img_61, img_62, img_63],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['S', 'L'],
+        date: 1716934345448,
+        bestseller: true,
+    },
+    {
+        id: '22',
+        name: 'product22',
+        description: 'product22 description',
+        price: 135,
+        image: [img_64, img_65, img_66],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['M', 'L'],
+        date: 1716634345448,
+        bestseller: false,
+    },
+    {
+        id: '23',
+        name: 'product23',
+        description: 'product23 description',
+        price: 145,
+        image: [img_67, img_68, img_69],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['S', 'M'],
+        date: 1716734345448,
+        bestseller: true,
+    },
+    {
+        id: '24',
+        name: 'product24',
+        description: 'product24 description',
+        price: 100,
+        image: [img_70, img_71, img_72],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['M', 'L'],
+        date: 1716834345448,
+        bestseller: false,
+    },
+    {
+        id: '25',
+        name: 'product25',
+        description: 'product25 description',
+        price: 115,
+        image: [img_73, img_74, img_75],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['S', 'M', 'L'],
+        date: 1716934345448,
+        bestseller: true,
+    },
+    {
+        id: '26',
+        name: 'product26',
+        description: 'product26 description',
+        price: 175,
+        image: [img_76, img_77, img_78],
+        category: 'Kids',
+        subCategory: 'Winterwear',
+        sizes: ['M', 'L'],
+        date: 1716634345448,
+        bestseller: false,
+    },
+    {
+        id: '27',
+        name: 'product27',
+        description: 'product27 description',
+        price: 85,
+        image: [img_79, img_80, img_81],
+        category: 'Men',
+        subCategory: 'Bottomwear',
+        sizes: ['S', 'L'],
+        date: 1716734345448,
+        bestseller: true,
+    },
+    {
+        id: '28',
+        name: 'product28',
+        description: 'product28 description',
+        price: 125,
+        image: [img_82, img_83, img_84],
+        category: 'Women',
+        subCategory: 'Topwear',
+        sizes: ['M', 'L'],
+        date: 1716834345448,
+        bestseller: false,
+    },
+]
